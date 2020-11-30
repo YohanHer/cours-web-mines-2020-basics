bdec5fdf570c8efcfa072b48f5c2d5398fb2a768
Yohan Her <yohan.hernandez5@etu.univ-lorraine.fr>
2020-11-30 16:27:54 +0100

    Ajout d'une interface pour le forum et récupération des messages sur le serveur

--- src/js/forum.js
+++ src/js/forum.js
@@ -1,15 +1,23 @@
 import ky from "ky";
+import $ from "jquery";
 
 function getMessageView(message) {
-  return `<div>
-    <p>${message.username}<p/>
-    <p>${message.message}</p>
-  </div>`;
+  return `<div class="card my-3">
+  <div class="card-body">
+      <p class="card-text">${message.content}</p>
+  </div>
+  <div class="card-footer text-muted text-right msg-origin">
+      By ${message.author}, ${message.timestamp}
+  </div>
+</div>`;
 }
 
 function displayMessages(messages) {
+  const $messageContainer = $("#card-container");
   // Clear list content on view
+  $messageContainer.empty();
   // Iterate on messages and display getMessageView(message);
+  $messageContainer.append(messages.map((message) => getMessageView(message)));
 }
 
 async function refreshMessages() {
@@ -18,11 +26,13 @@ async function refreshMessages() {
   displayMessages(messages);
 }
 
-setInterval(() => {
-  refreshMessages();
-}, 1000);
+refreshMessages();
 
-function sendMessage(message) {
-  // POST https://ensmn.herokuapp.com/messages (username, message)
-  // After success, getMessages()
-}
+//  setInterval(() => {
+//  refreshMessages();
+//  }, 1000);
+
+//  function sendMessage(message) {
+// POST https://ensmn.herokuapp.com/messages (username, message)
+// After success, getMessages()
+//  }
