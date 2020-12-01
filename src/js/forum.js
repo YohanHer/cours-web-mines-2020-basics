69ea59a5e6f8d7648139d5cd5acad09f00966e36
Yohan Her <yohan.hernandez5@etu.univ-lorraine.fr>
2020-12-01 11:26:37 +0100

    Ajout de l'envoi de messages

--- src/js/forum.js
+++ src/js/forum.js
@@ -21,18 +21,55 @@ function displayMessages(messages) {
 }
 
 async function refreshMessages() {
+  let pageIndex = 0;
+  let hasMessages = true;
+  let messages = [];
+
   // GET https://ensmn.herokuapp.com/messages
-  const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
+  while (hasMessages) {
+    // eslint-disable-next-line
+    const pageMessages = await ky
+      .get(`https://ensmn.herokuapp.com/messages?page=${pageIndex}`)
+      .json();
+    hasMessages = pageMessages.length > 0;
+    pageIndex += 1;
+    messages = messages.concat(pageMessages);
+  }
+
   displayMessages(messages);
 }
 
 refreshMessages();
 
-//  setInterval(() => {
-//  refreshMessages();
-//  }, 1000);
+setInterval(() => {
+  refreshMessages();
+}, 1000);
 
-//  function sendMessage(message) {
-// POST https://ensmn.herokuapp.com/messages (username, message)
-// After success, getMessages()
-//  }
+function sendMessage(message) {
+  // POST https://ensmn.herokuapp.com/messages (username, message)
+  ky.post("https://ensmn.herokuapp.com/messages", { json: message });
+  // After success, getMessages()
+  refreshMessages();
+
+  $("#message").val("");
+}
+
+$("body").on("submit", "#message-form", (event) => {
+  event.preventDefault();
+  const $form = $("#message-form");
+  const data = $form.serializeArray();
+  const messageData = {};
+  data.forEach(({ name, value }) => {
+    messageData[name] = value;
+  });
+
+  if (
+    messageData.username == null ||
+    messageData.username.length === 0 ||
+    messageData.message == null ||
+    messageData.message === 0
+  ) {
+    return;
+  }
+  sendMessage(messageData);
+});
